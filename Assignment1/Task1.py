79062ac089ecc1a7e1bf2f8eb511d8372abad7ef
luciafabbri <luciafabbri1999@gmail.com>
2023-10-19 01:01:07 +0200

    new comments added task1

--- Assignment1/Task1.py
+++ Assignment1/Task1.py
@@ -8,11 +8,20 @@ from gensim import similarities
 from nltk.corpus import stopwords
 from nltk import PorterStemmer
 
-# Initiating the time for the execution time calculation
+
+### PART 1
+# Data Preprocessing: 
+# - Measure the starting execution time for the model
+# - CSV import, extraction and saving all the results in different variables
+# - Remove all the stopwords inside the 'description' column 
+# - Create a dictionary with the words and the relative ids
+### 
+
+# Starting time for the subprocess 'creating the model'
 init_t: datetime = datetime.datetime.now()
 
 # CSV extraction
-news_file = "news.csv"
+news_file = "./news.csv"
 
 all_news = []
 descriptions = []
@@ -30,13 +39,12 @@ with open(news_file, 'r', newline='', encoding='utf-8') as csv_file:
 
 num_articles_food_and_drink = len(food_drink_descriptions)
 
-# Delete the first row 'description' name
+# Delete the first CSV row of the column 'description'
 descriptions.pop(0)
 
 porter = PorterStemmer()
-# Remove common words and tokenize
+# Remove common words using stopwords 
 stoplist = stopwords.words('english')
-
 # Array of list of words for each document
 texts = [
     [porter.stem(word) for word in document.lower().split() if word not in stoplist]
@@ -45,15 +53,28 @@ texts = [
 
 # create mapping keyword-id
 dictionary = corpora.Dictionary(texts)
-
 model_bow = [dictionary.doc2bow(text) for text in texts]
 
-# create tfidf model
+
+### PART 2
+# - Create the TF-IDF model 
+# - Measure the ending execution time for the model
+### 
+
 tfidf = models.TfidfModel(model_bow)
 tfidf_vectors = tfidf[model_bow]
-matrix_tfidf = similarities.MatrixSimilarity(tfidf_vectors)  # this matrix will be necessary to calculate similarity between documents
+# The following matrix will be necessary to calculate similarity between documents
+matrix_tfidf = similarities.MatrixSimilarity(tfidf_vectors)
 
-end_creation_model_t: datetime = datetime.datetime.now()  # just after the calculation of the matrix similarity -> time function
+# Ending time for the subprocess 'creating the model' + Starting time for the subprocess 'pseudocode'
+end_creation_model_t: datetime = datetime.datetime.now() 
+
+
+### PART 3
+# - Create the function based on the pseudocode 
+# - Apply the function the 
+# - Measure the ending execution time for the subprocess 'pseudocode'
+### 
 
 total_goods = 0
 
@@ -93,11 +114,59 @@ print("total_goods = ", total_goods)
 print("num_articles_food_and_drink = ", num_articles_food_and_drink)
 print("ratio_quality = ", ratio_quality)
 
-end_t: datetime = datetime.datetime.now()  # to mark the end of the program
 
-# Get execution time
+'''
+
+def calculate_quality_ratio(food_drink_descriptions, all_news, num_articles_food_and_drink):
+    
+    total_goods = 0
+    
+    # Filtering the food and drink descriptions with stopwords and other regex expressions
+    for food_drink_description in food_drink_descriptions:
+        doc_s = [porter.stem(word) for word in food_drink_description.lower().split() if word not in stoplist]
+
+        vec_bow = dictionary.doc2bow(doc_s)
+        vec_tfidf = tfidf[vec_bow]
+    
+        # Calculating similarities between doc and each doc of texts using tfidf vectors and cosine
+        sims = matrix_tfidf[vec_tfidf]
+
+        # Sorting similarities in descending order
+        sims = sorted(enumerate(sims), key=lambda item: -item[1])
+
+        # Selecting the 10 most similar elements
+        top_10_similar_elements = sims[:10]
+
+        goods = 0
+        for doc_position, doc_score in top_10_similar_elements:
+            if all_news[doc_position][2] == "Food & Drink":
+                goods += 1
+
+        total_goods += goods
+
+    ratio_quality = total_goods / (num_articles_food_and_drink * 10)
+
+    print("total_goods =", total_goods)
+    print("num_articles_food_and_drink =", num_articles_food_and_drink)
+    print("ratio_quality =", ratio_quality)
+
+    return ratio_quality
+
+# Usage
+food_drink_descriptions = [...]  # Your list of food and drink descriptions
+all_news = [...]  # Your list of all news data
+num_articles_food_and_drink = ...  # The number of articles related to food and drink
+
+quality_ratio = calculate_quality_ratio(food_drink_descriptions, all_news, num_articles_food_and_drink)
+
+'''
+
+# Ending time for the subprocess 'pseudocode' (but also the programm in general)
+end_t: datetime = datetime.datetime.now() 
+
+# Get the execution time for both the two subprocesses
 elapsed_time_model_creation: datetime = end_creation_model_t - init_t
-elapsed_time_comparison: datetime = end_t - end_creation_model_t
+elapsed_time_pseudocode: datetime = end_t - end_creation_model_t
 print()
 print('Execution time model:', elapsed_time_model_creation, 'seconds')
-print('Execution time comparison:', elapsed_time_comparison, 'seconds')
+print('Execution time comparison:', elapsed_time_pseudocode, 'seconds')
