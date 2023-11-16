041bcedf2d1c7dd8a8206a47596cf3e5938ec23a
MathildeMrz <mathilde.marza2001@gmail.com>
2023-11-16 16:16:18 +0100

    regrouped tasks

--- Assignment2/Tasks1_2_3.py
+++ Assignment2/Tasks1_2_3.py
@@ -5,17 +5,56 @@ from surprise.accuracy import mae
 from surprise.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from surprise import Reader, SVD
-from Task1 import precision_recall_at_n
-from Task1 import usersNumber
 
 # Load the movielens-100k dataset (download it if needed).
 data = Dataset.load_builtin('ml-100k')
-
+# usersNumber = data.build_full_trainset().n_users
+usersNumber = 100
+print(f"Users number : {usersNumber}")
 mae_values = []
 best_k = None
+n_values = range(10, 1000)
 
 
-def train_test_knn(size):
+def precision_recall_at_n(predictions, n=10, threshold=4):
+    """Return precision and recall at n metrics for each user"""
+
+    # First map the predictions to each user.
+    user_est_true = defaultdict(list)
+    for uid, _, true_r, est, _ in predictions:
+        user_est_true[uid].append((est, true_r))
+
+    precisions = dict()
+    recalls = dict()
+    for uid, user_ratings in user_est_true.items():
+        # Sort user ratings by estimated value
+        user_ratings.sort(key=lambda x: x[0], reverse=True)
+
+        # Number of relevant items
+        n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
+
+        # Number of relevant and recommended items in top n
+        n_rel_and_rec = sum(
+            (true_r >= threshold)
+            for (_, true_r) in user_ratings[:n]
+        )
+
+        # Precision@n: Proportion of recommended items that are relevant
+        # When n_rec_k is 0, Precision is undefined. We here set it to 0.
+
+        precisions[uid] = n_rel_and_rec / n
+
+        # Recall@n: Proportion of relevant items that are recommended
+        # When n_rel is 0, Recall is undefined. We here set it to 0.
+
+        recalls[uid] = n_rel_and_rec / n_rel if n_rel != 0 else 0
+
+    return precisions, recalls
+
+
+def train_test_kdd(size):
+    mae_values = []  # Reset mae_values for each sparsity level
+
     precision_values = []
     recall_values = []
 
@@ -46,15 +85,22 @@ def train_test_knn(size):
             min_mae = current_mae
             best_k = k
 
-    best_algo = KNNWithMeans(best_k, sim_options=sim_options_KNN, verbose=False)
-    best_algo.fit(trainset)
-    predictionsKNN_best = best_algo.test(testset)
-
     print(f"Sparcity: {size}%")
     print(f"Best K for minimizing MAE: {best_k}")
     print(f"Lowest MAE: {min_mae}")
 
-    n_values = range(10, 1000)
+    plt.plot(k_values_to_test, mae_values, marker='o', label='MAE values')
+    plt.scatter(best_k, min_mae, color='red', label=f'Best K={best_k}\nLowest MAE={min_mae}', zorder=5)
+    plt.title(f'MAE values for different k (Sparsity: {size}%)')  # Updated line
+    plt.xlabel('k values')
+    plt.ylabel('MAE')
+    plt.show()
+
+    # -----------------------------Task 3----------------------------- #
+
+    best_algo = KNNWithMeans(best_k, sim_options=sim_options_KNN, verbose=False)
+    best_algo.fit(trainset)
+    predictionsKNN_best = best_algo.test(testset)
 
     for n in n_values:
         precisions, recalls = precision_recall_at_n(predictionsKNN_best, n=n, threshold=4)
@@ -82,20 +128,26 @@ def train_test_knn(size):
 
 
 def train_test_svd(size):
-    precision_values = []
-    recall_values = []
     # Dataset splitting in trainset and testset for size% sparsity
     trainset, testset = train_test_split(data, test_size=size / 100,
                                          random_state=22)
+    precision_values = []
+    recall_values = []
+
     # prepare user-based SVD for predicting ratings from trainset
     algo = SVD(random_state=3)
     algo.fit(trainset)
+
+    # estimate the ratings for all the pairs (user, item) in testset
     predictionsSVD = algo.test(testset)
+    current_mae = mae(predictionsSVD)
 
-    n_values = range(10, 1000)
+    print(f"Sparcity: {size}%")
+    print(f"MAE: {current_mae}")
 
+    # -----------------------------Task 3----------------------------- #
     for n in n_values:
-        precisions, recalls = precision_recall_at_n(predictionsSVD, n=5, threshold=4)
+        precisions, recalls = precision_recall_at_n(predictionsSVD, n=n, threshold=4)
         # Precision and recall can then be averaged over all users
         pre = sum(prec for prec in precisions.values()) / len(precisions)
         recall = sum(rec for rec in recalls.values()) / len(recalls)
@@ -118,13 +170,13 @@ def train_test_svd(size):
     plt.show()
 
 
-# KNN
+# -----------------------------Task 1----------------------------- #
 # Sparcity of 25%
-train_test_knn(25)
+train_test_kdd(25)
 # Sparcity of 75%
-train_test_knn(75)
+train_test_kdd(75)
 
-# SVD
+# -----------------------------Task 2----------------------------- #
 # Sparcity of 25%
 train_test_svd(25)
 # Sparcity of 75%
